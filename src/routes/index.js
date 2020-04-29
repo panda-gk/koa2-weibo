8ad77630e94e918e9aae0cd377dda57672402f74
mockuai <mockuai@mockuaideMacBook-Pro.local>
2020-04-29 10:52:05 +0800

    refactor:调整目录结构

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
+  console.log(process.env)
+
   await ctx.render('index', {
     title: 'Hello Koa 2!'
   })
 })
 
 router.get('/string', async (ctx, next) => {
+
   ctx.body = 'koa2 string'
 })
 
